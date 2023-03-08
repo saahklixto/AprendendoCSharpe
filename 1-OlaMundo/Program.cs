fb288e405b73166a0011c53cc74082f25dd62c85
Samara <Samara@DESKTOP-R1UELAH>
2023-03-08 14:08:53 -0300

    Adicionar arquivos de projeto.

---
+++ 1-OlaMundo/Program.cs
@@ -0,0 +1,18 @@
+﻿
+using System;
+
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("tecle enter para fechar");
+        Console.ReadLine();
+
+    }
+
+
+
+    // See https://aka.ms/new-console-template for more information
+
+}
---
+++ 2-criandoVariavel/Program.cs
@@ -0,0 +1,26 @@
+﻿
+using System;
+
+class Programa
+{
+    static void Main(string[] args)
+    {
+        int idade;
+
+        idade = 27;
+        Console.WriteLine(idade);
+
+        idade = 27 - 5;
+        Console.WriteLine(idade);
+        idade = 5 * 2 - 6;
+        Console.WriteLine(idade);
+
+        idade = (5 - 2) * 3;
+
+        Console.WriteLine(idade);
+    }
+
+
+
+
+}
